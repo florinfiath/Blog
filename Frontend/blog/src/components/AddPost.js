1aa129b2128fdcb432c3df8de2a0e3ed5f6acd61
Florin <fiathf@yahoo.com>
2021-05-11 15:49:06 +0200

    Add Post component in work

--- Frontend/blog/src/components/AddPost.js
+++ Frontend/blog/src/components/AddPost.js
@@ -3,9 +3,32 @@ import React from 'react';
 
 const AddPost = () => {
     return (
-        <div>
-            Add Post
-        </div>
+      <div className="">
+        <form>
+          <div class="form-group">
+            <h3>Add a New Post</h3>
+            <p>Title:</p>
+            <input
+              type="text"
+              class="form-control"
+              id="addTitlePost"
+              placeholder="Enter post title"
+            />
+          </div>
+          <div class="form-group">
+            <p>Content:</p>
+            <input
+              type="text"
+              class="form-control"
+              id="addContent"
+              placeholder="add content"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
     );
 };
 
